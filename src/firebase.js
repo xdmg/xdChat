78a49a4d5e5b7b4c0ee337ca7526e4b2bf3f8308
Henry <captainhenry971@gmail.com>
2022-09-21 19:39:46 +0530

    test

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC27igli5WRikxRfAypds_wK9Q4ZENDbxM",
+    authDomain: "wbca-mgxd.firebaseapp.com",
+    projectId: "wbca-mgxd",
+    storageBucket: "wbca-mgxd.appspot.com",
+    messagingSenderId: "1079429386538",
+    appId: "1:1079429386538:web:f3c3b72d0f854140d14e53"
+};
